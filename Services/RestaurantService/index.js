8c62b0e1d9a6d61688c127e0bcf7c7cf19ca2f4d
Shanuka Induran <shanukainduran@gmail.com>
2025-05-25 21:23:40 +0530

    Updated

--- Services/RestaurantService/index.js
+++ Services/RestaurantService/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const restaurantRoutes = require('./routes/restaurantRoutes');
 const connectDB = require('./config/db');
 
 dotenv.config();
 
 const app = express();
+app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(express.json());
 
-app.use('/api', restaurantRoutes); // Changed from '/'
+app.use('/api', restaurantRoutes);
 
 connectDB();
 
