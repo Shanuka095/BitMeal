1039ef593881284e717614e23b61ffc2744fa2d4
Shanuka Induran <shanukainduran@gmail.com>
2025-05-25 17:41:32 +0530

    Import index.css in the main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import '../index.css';
 
 console.log('Starting React render...');
 const rootElement = document.getElementById('root');
