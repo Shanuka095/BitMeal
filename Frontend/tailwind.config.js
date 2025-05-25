c49062fd58968cd9d06b99c51edfb77e15bab3bb
Shanuka Induran <shanukainduran@gmail.com>
2025-05-25 18:09:11 +0530

    Update

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}",
+    './index.html', // Scan index.html in Frontend
+    './src/**/*.{js,jsx,ts,tsx}', // Scan all JS/JSX files in src
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
