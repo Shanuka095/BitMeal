d951331153efaedf494723d967467f7520e317b1
Shanuka Induran <shanukainduran@gmail.com>
2025-06-08 00:26:34 +0530

    Update UIs and background color and any other features

--- Services/RestaurantService/index.js
+++ Services/RestaurantService/index.js
@@ -7,7 +7,7 @@ const connectDB = require('./config/db');
 dotenv.config();
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', restaurantRoutes);
