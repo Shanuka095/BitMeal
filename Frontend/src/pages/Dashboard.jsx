4a7da8b3b5bbfe8f94e1163a0e97d38c9c807e97
Shanuka Induran <shanukainduran@gmail.com>
2025-06-08 13:25:45 +0530

    Update Dashboard.jsx

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -5,10 +5,10 @@ import Restaurants from './Restaurants';
 
 const Dashboard = () => {
   return (
-    <div className="w-screen min-h-screen bg-[#fffce5] font-sans flex flex-col">
+    <div className="w-screen min-h-screen bg-[#e3e3e3] font-sans flex flex-col">
       <Navbar />
-      <div className="flex-grow pt-16">
-        <Restaurants />
+      <div className="flex-grow pt-20">
+        <Restaurants standalone={false} />
       </div>
       <Footer />
     </div>
