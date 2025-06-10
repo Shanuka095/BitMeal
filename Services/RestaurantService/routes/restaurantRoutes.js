7a124049df55315b9e7305bd34189ff4250f60d3
Shanuka Induran <shanukainduran@gmail.com>
2025-06-10 12:31:05 +0530

    Update restaurantRoutes.js

--- Services/RestaurantService/routes/restaurantRoutes.js
+++ Services/RestaurantService/routes/restaurantRoutes.js
@@ -1,3 +1,4 @@
+// src/routes/restaurantRoutes.js
 const express = require('express');
 const router = express.Router();
 const {
@@ -15,7 +16,7 @@ const { authenticate, restrictTo } = require('../middleware/restrictAccess');
 router.post('/restaurants', authenticate, restrictTo('restaurant_admin'), validateRestaurant, createRestaurant);
 router.put('/restaurants/:id', authenticate, restrictTo('restaurant_admin'), validateRestaurant, updateRestaurant);
 router.delete('/restaurants/:id', authenticate, restrictTo('restaurant_admin'), removeRestaurant);
-router.post('/restaurants/:restaurantId/menu', authenticate, restrictTo('restaurant_admin'), validateMenuItem, addMenuItem);
+router.post('/restaurants/:restaurantId/menu', authenticate, restrictTo('restaurant_admin'), validateMenuItem, addMenuItem); // Ensure correct order
 router.get('/restaurants', getRestaurants);
 router.get('/restaurants/:id', getRestaurantById);
 
