997dcdb23176fa05b2041f72ad5408ae43d5eaeb
Shanuka Induran <shanukainduran@gmail.com>
2025-06-28 23:37:54 +0530

    Update Home.jsx

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,4 +1,3 @@
-// src/pages/Home.jsx
 import { Link } from 'react-router-dom';
 
 const Home = () => {
@@ -7,10 +6,10 @@ const Home = () => {
       <h1 className="text-4xl font-bold text-[#4f4f4f] mb-6">Welcome to BitMeal</h1>
       <p className="text-lg text-[#4f4f4f] mb-8">Please sign in or register to continue.</p>
       <div className="space-x-4">
-        <Link to="/login" className="px-4 py-2 bg-[#ffaa00] text-white rounded hover:bg-[#cc8800]">
+        <Link to="/login" className="px-4 py-2 bg-[#ffaa00] text-white rounded hover:bg-[#cc8800] transition">
           Sign In
         </Link>
-        <Link to="/register" className="px-4 py-2 bg-[#ffaa00] text-white rounded hover:bg-[#cc8800]">
+        <Link to="/register" className="px-4 py-2 bg-[#ffaa00] text-white rounded hover:bg-[#cc8800] transition">
           Sign Up
         </Link>
       </div>
