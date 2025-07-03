f9b04db19c969b97ce92ea2788a1af990c31907d
Shanuka Induran <shanukainduran@gmail.com>
2025-07-03 22:56:40 +0530

    Update restaurantModel.js

--- Services/RestaurantService/models/restaurantModel.js
+++ Services/RestaurantService/models/restaurantModel.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const menuItemSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  price: { type: Number, required: true, min: 0 },
+  normalPrice: { type: Number, required: true, min: 0 }, // Changed from 'price'
+  extraPriceForFull: { type: Number, default: 0, min: 0 }, // New field for 'Full' size price increase
   category: { type: String, required: true },
   imageUrl: { type: String, default: '' }, // Optional image URL for menu item
 });
