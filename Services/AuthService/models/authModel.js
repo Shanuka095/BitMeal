590e93c169aa129cc5955d3bbe8ea4d77d3d21a9
Shanuka Induran <shanukainduran@gmail.com>
2025-07-13 02:25:49 +0530

    Update authModel.js

--- Services/AuthService/models/authModel.js
+++ Services/AuthService/models/authModel.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 const authSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  phone: { type: String, required: true },
+  name: { type: String, required: true }, // <-- ADD THIS LINE
   role: { type: String, enum: ['customer', 'restaurant_admin', 'delivery_personnel'], default: 'customer' },
   isVerified: { type: Boolean, default: false },
-  otp: { type: String }, // Store hashed OTP
-  otpExpires: { type: Date }, // OTP expiration time
-});
+  otp: { type: String },
+  otpExpires: { type: Date },
+}, { timestamps: true });
 
 module.exports = mongoose.model('Auth', authSchema);
