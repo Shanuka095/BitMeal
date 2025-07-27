218cf337255b01e3cc4213793740debee7865678
Shanuka Induran <shanukainduran@gmail.com>
2025-07-27 20:12:02 +0530

    Update userRoutes.js

--- Services/UserService/routes/userRoutes.js
+++ Services/UserService/routes/userRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getProfile, updateProfile } = require('../controllers/userController');
+const { getProfile, updateProfile, createProfile } = require('../controllers/userController'); // <-- IMPORT new controller
 
+router.post('/create-profile', createProfile); // <-- ADD THIS LINE (no auth needed, called internally)
 router.get('/profile', getProfile);
 router.put('/profile', updateProfile);
 
