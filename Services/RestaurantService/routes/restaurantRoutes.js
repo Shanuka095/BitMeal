80a11c834590f53da7ab659cf002841b45aed070
Shanuka Induran <shanukainduran@gmail.com>
2025-08-19 15:00:38 +0530

    Update restaurantRoutes.js

--- Services/RestaurantService/routes/restaurantRoutes.js
+++ Services/RestaurantService/routes/restaurantRoutes.js
@@ -1,38 +1,37 @@
-    const express = require('express');
-    const router = express.Router();
-    const {
-      getPublicRestaurants,
-      getPublicRestaurantDetails,
-      getAdminRestaurants,
-      createRestaurant,
-      updateRestaurant,
-      deleteRestaurant,
-      addMenuItem,
-      getRestaurantDetails,
-      getMenuItem,
-      updateMenuItem,
-      deleteMenuItem,
-      submitRating, // NEW
-    } = require('../controllers/restaurantController');
-    const { authenticate, restrictTo } = require('../middleware/restrictAccess');
+const express = require('express');
+const router = express.Router();
+const {
+  getPublicRestaurants,
+  getPublicRestaurantDetails,
+  getAdminRestaurants,
+  createRestaurant,
+  updateRestaurant,
+  deleteRestaurant,
+  addMenuItem,
+  getRestaurantDetails,
+  getMenuItem,
+  updateMenuItem,
+  deleteMenuItem,
+  submitRating,
+} = require('../controllers/restaurantController');
+const { authenticate, restrictTo } = require('../middleware/restrictAccess');
 
-    // Customer: Public access
-    router.get('/public', getPublicRestaurants);
-    router.get('/public/:id', getPublicRestaurantDetails);
+// Customer: Public access
+router.get('/public', getPublicRestaurants);
+router.get('/public/:id', getPublicRestaurantDetails);
 
-    // NEW: Route for submitting restaurant ratings
-    router.post('/:id/rate', authenticate, restrictTo('customer'), submitRating);
+// Route for submitting restaurant ratings
+router.post('/:id/rate', authenticate, restrictTo('customer'), submitRating);
 
-    // Admin: Protected routes
-    router.get('/', authenticate, restrictTo('restaurant_admin'), getAdminRestaurants);
-    router.get('/:id', authenticate, restrictTo('restaurant_admin'), getRestaurantDetails);
-    router.get('/:id/menu/:menuId', authenticate, restrictTo('restaurant_admin'), getMenuItem);
-    router.post('/', authenticate, restrictTo('restaurant_admin'), createRestaurant);
-    router.put('/:id', authenticate, restrictTo('restaurant_admin'), updateRestaurant);
-    router.delete('/:id', authenticate, restrictTo('restaurant_admin'), deleteRestaurant);
-    router.post('/:id/menu', authenticate, restrictTo('restaurant_admin'), addMenuItem);
-    router.put('/:id/menu/:menuId', authenticate, restrictTo('restaurant_admin'), updateMenuItem);
-    router.delete('/:id/menu/:menuId', authenticate, restrictTo('restaurant_admin'), deleteMenuItem);
+// Admin: Protected routes
+router.get('/', authenticate, restrictTo('restaurant_admin'), getAdminRestaurants);
+router.get('/:id', authenticate, restrictTo('restaurant_admin'), getRestaurantDetails);
+router.get('/:id/menu/:menuId', authenticate, restrictTo('restaurant_admin'), getMenuItem);
+router.post('/', authenticate, restrictTo('restaurant_admin'), createRestaurant);
+router.put('/:id', authenticate, restrictTo('restaurant_admin'), updateRestaurant);
+router.delete('/:id', authenticate, restrictTo('restaurant_admin'), deleteRestaurant);
+router.post('/:id/menu', authenticate, restrictTo('restaurant_admin'), addMenuItem);
+router.put('/:id/menu/:menuId', authenticate, restrictTo('restaurant_admin'), updateMenuItem);
+router.delete('/:id/menu/:menuId', authenticate, restrictTo('restaurant_admin'), deleteMenuItem);
 
-    module.exports = router;
-    
+module.exports = router;
